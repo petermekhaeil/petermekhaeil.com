6193c63e992aebdf90fa19a9b9215e4ea784a009
Peter Mekhaeil <mekhaeil.peter@gmail.com>
2020-11-21 23:14:22 +0800

    use postcss purge and minify

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require("tailwindcss"),
+    process.env.NODE_ENV === "production" ? require("autoprefixer") : null,
+    process.env.NODE_ENV === "production"
+      ? cssnano({ preset: "default" })
+      : null,
+    purgecss({
+      content: ["./src/**/*.njk", "./src/**/*.md", "./*.js"],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
+  ],
 };
