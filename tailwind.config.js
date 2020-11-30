d5dfbda731021d3e683f70023cffc732442a3233
Peter Mekhaeil <mekhaeil.peter@gmail.com>
2020-11-30 22:34:16 +0800

    add prettier + code dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,66 +1,69 @@
 module.exports = {
   purge: {
-    content: ["./src/**/*.njk", "./src/**/*.md", "./*.js"],
+    content: ['./src/**/*.njk', './src/**/*.md', './*.js']
   },
-  darkMode: "class", // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       typography: (theme) => ({
         DEFAULT: {
           css: {
-            color: theme("colors.gray.900"),
+            color: theme('colors.gray.900'),
             a: {
-              color: theme("colors.blue.600"),
-              "&:hover": {
-                color: theme("colors.blue.600"),
-                textDecoration: "none",
+              color: theme('colors.blue.600'),
+              '&:hover': {
+                color: theme('colors.blue.600'),
+                textDecoration: 'none'
               },
-              textDecoration: "none",
-            },
-          },
+              textDecoration: 'none'
+            }
+          }
         },
 
         dark: {
           css: {
-            color: theme("colors.gray.200"),
+            color: theme('colors.gray.200'),
             a: {
-              color: theme("colors.yellow.400"),
-              "&:hover": {
-                color: theme("colors.yellow.400"),
-                textDecoration: "none",
-              },
+              color: theme('colors.yellow.400'),
+              '&:hover': {
+                color: theme('colors.yellow.400'),
+                textDecoration: 'none'
+              }
             },
             h1: {
-              color: theme("colors.blue.300"),
+              color: theme('colors.blue.300')
             },
             h2: {
-              color: theme("colors.blue.300"),
+              color: theme('colors.blue.300')
             },
             h3: {
-              color: theme("colors.blue.300"),
+              color: theme('colors.blue.300')
             },
             h4: {
-              color: theme("colors.blue.300"),
+              color: theme('colors.blue.300')
             },
             h5: {
-              color: theme("colors.blue.300"),
+              color: theme('colors.blue.300')
             },
             h6: {
-              color: theme("colors.blue.300"),
+              color: theme('colors.blue.300')
             },
             strong: {
-              color: theme("colors.blue.300"),
+              color: theme('colors.blue.300')
             },
             blockquote: {
-              color: theme("colors.blue.300"),
+              color: theme('colors.blue.300')
             },
-          },
-        },
-      }),
-    },
+            code: {
+              color: theme('colors.gray.300')
+            }
+          }
+        }
+      })
+    }
   },
   variants: {
-    typography: ["responsive", "dark"],
+    typography: ['responsive', 'dark']
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require('@tailwindcss/typography')]
 };
