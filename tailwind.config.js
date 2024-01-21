3fa076c9785acc2b985952dfb8029a0ecb5b63e5
Peter Mekhaeil <4616064+petermekhaeil@users.noreply.github.com>
2024-01-21 23:28:43 +0800

    use Inter from node modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+        sans: ['InterVariable', 'Inter', ...defaultTheme.fontFamily.sans]
       }
     }
   },
