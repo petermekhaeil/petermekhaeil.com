083a56ea815a4f29b1b261d3df62b5cd270bfcf2
Peter Mekhaeil <4616064+petermekhaeil@users.noreply.github.com>
2024-01-28 00:39:41 +0800

    add geolocation

--- src/pages/api/page-view.ts
+++ src/pages/api/page-view.ts
@@ -9,12 +9,14 @@ export const POST: APIRoute = async ({ request, locals }) => {
     const date = new Date();
     const { path } = await new Response(request.body).json();
 
+    const { country, city, latitude, longitude } =  locals.netlify.context.geo;
+
     if (!path) {
         return Response.json({ message: 'Missing path.' });
     } else {
         await sql(
-            'INSERT INTO analytics(date, path) VALUES($1, $2)',
-            [date, path]
+            'INSERT INTO analytics(date, path, country, city, latitude, longitude) VALUES($1, $2, $3, $4, $5, $6)',
+            [date, path, country, city, latitude, longitude]
         );
 
         return Response.json({ context: locals.netlify.context.geo });
