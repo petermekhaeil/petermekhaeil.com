ffaee42f9ab2a6b5babfd0be8476859c6a23ef44
Peter Mekhaeil <4616064+petermekhaeil@users.noreply.github.com>
2024-02-01 22:42:41 +0800

    prerender static pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
