6144016439ecf27c76d951a90f231fb24e024483
Peter Mekhaeil <4616064+petermekhaeil@users.noreply.github.com>
2025-01-05 22:48:58 +0800

    upgrade shiki

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,34 +1,60 @@
 import { marked } from 'marked';
-import shiki from 'shiki';
+import { createHighlighter, type Highlighter } from 'shiki';
+import { transformerStyleToClass } from '@shikijs/transformers';
 
-export function markdownToHtml(markdown: string) {
-  return new Promise((resolve, reject) => {
+const toClass = transformerStyleToClass({
+  classPrefix: '__shiki_'
+});
+
+let highlighterInstance: Highlighter | null = null;
+
+async function getSingletonHighlighter(): Promise<Highlighter> {
+  if (!highlighterInstance) {
+    highlighterInstance = await createHighlighter({
+      themes: ['github-dark'],
+      langs: [
+        'javascript',
+        'typescript',
+        'html',
+        'css',
+        'json',
+        'bash',
+        'dart',
+        'graphql',
+        'yaml',
+        'sql',
+        'ini',
+        'docker',
+        'php',
+        'cs'
+      ]
+    });
+  }
+  return highlighterInstance;
+}
+
+export async function markdownToHtml(markdown: string) {
+  return new Promise(async (resolve, reject) => {
+    const highlighter = await getSingletonHighlighter();
+
     const markedOptions: marked.MarkedOptions = {
       highlight: (code, lang, callback) => {
-        shiki
-          .getHighlighter({ theme: 'github-dark' })
-          .then((highlighter) => {
-            const tokens = highlighter.codeToThemedTokens(code, lang);
-            const html = shiki.renderToHtml(tokens, {
-              elements: {
-                pre({ children }) {
-                  return `${children}`;
-                },
-                code({ children }) {
-                  return `${children}`;
-                }
+        const html = highlighter.codeToHtml(code, {
+          lang,
+          theme: 'github-dark',
+          transformers: [
+            toClass,
+            {
+              pre(node) {
+                this.addClassToHast(node, 'not-prose m-0 p-0');
               }
-            });
+            }
+          ]
+        });
 
-            if (callback) {
-              callback(null, html);
-            }
-          })
-          .catch((error) => {
-            if (callback) {
-              callback(error);
-            }
-          });
+        if (callback) {
+          callback(null, html);
+        }
       }
     };
 
